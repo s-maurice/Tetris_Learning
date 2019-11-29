ba3cd22bd3195ae190a318f5c33480b100759593
hYm3nSl4yr <imauriceinoob@gmail.com>
2019-11-30 03:46:43 +0800

    prepare_model_inputs now accepts list of dicts for batching in preparation for model training model changes

--- tetris_dqn.py
+++ tetris_dqn.py
@@ -12,13 +12,14 @@ from TetrisGame import TetrisGame
 from DrawBoard import DrawBoard
 
 
-def prepare_model_inputs(input_dict: dict):
-    # takes game_state dict and returns the list of inputs with the correct shapes for the model ot use as inputs
+def prepare_model_inputs(input_dicts):
+    # takes list of game_state dict - list size is the batch size, returns a list of batched elements for model input
+
     # define the keys for the inputs
-    keys_board = ["placed_board", "combined_board", "current_tetris_board", "dropped_board"]
-    keys_tetris = ["current_tetris_4x4", "saved_tetris_4x4"]
-    keys_gaps = ["top_line_gaps"]
-    keys_one_dim = ["game_tick_index",
+    keys_board = ("placed_board", "combined_board", "current_tetris_board", "dropped_board")
+    keys_tetris = ("current_tetris_4x4", "saved_tetris_4x4")
+    keys_gaps = ("top_line_gaps", )
+    keys_one_dim = ("game_tick_index",
                     "lines_cleared",
                     "pos_x",
                     "pos_y",
@@ -33,26 +34,30 @@ def prepare_model_inputs(input_dict: dict):
                     "board_fill_percentage",
                     "board_height_percentage",
                     "top_line_fill_percentage",
-                    "move_hold_valid"]
+                    "move_hold_valid")
+    # store into a list for iteration
+    input_type_keys = [keys_board, keys_tetris, keys_gaps, keys_one_dim]
 
-    # get and prepare the board inputs
-    boards = input_dict.get(keys_board)
-    board_input = np.concatenate(boards)
+    # handle batch size of 0 passed as singular dict
+    if type(input_dicts) == dict:
+        input_dicts = [input_dicts]
 
-    # get and prepare the tetris inputs
-    tetris = input_dict.get(keys_tetris)
-    tetris_input = np.concatenate(tetris)
+    # prepare and store into list for iteration the lists for the inputs to be placed into
+    board_input, tetris_input, gap_input, one_dim_input = [], [], [], []
+    input_type_lists = [board_input, tetris_input, gap_input, one_dim_input]
 
-    # get and prepare the gap inputs
-    gap_input = input_dict.get(keys_gaps)
+    # separate the list of dicts into list batches of each input
+    for batch_dict in input_dicts:
+        for input_type_list, input_type_key in zip(input_type_lists, input_type_keys):
+            input_type_list.append([batch_dict.get(i) for i in input_type_key])  # dict.get() doesnt accept lists
 
-    # get and prepare the one_dim inputs
-    one_dim_input = input_dict.get(keys_one_dim)
+    # concatenate each list batch into (batchsize, ...) shape - axis 0 is the batch axis
+    # reshape the inputs to have a batch axis at position 0
+    input_type_lists = [np.expand_dims(i, axis=0) for i in input_type_lists]
+    # concat along axis 0 to group batches up
+    input_type_lists = [np.concatenate(i, axis=0) for i in input_type_lists]
 
-    # debug print
-    [print(i) for i in [board_input, tetris_input, gap_input, one_dim_input]]
-
-    return [board_input, tetris_input, gap_input, one_dim_input]
+    return input_type_lists
 
 
 game = TetrisGame()
@@ -93,20 +98,20 @@ action_space = [lambda: game.move_rotate(1),
 
 # define model using functional api
 # inputs
-in_boards = Input(shape=(10, 24, 4), name="in_boards")  # placed, combined, current_tetris, dropped - boards
-in_tetris = Input(shape=(4, 4, 2), name="in_tetris")  # current_tetris_4x4, saved_tetris_4x4
-in_gaps = Input(shape=(10, 1), name="in_gaps")  # top_line_gaps
+in_boards = Input(shape=(4, 10, 24), name="in_boards")  # placed, combined, current_tetris, dropped - boards
+in_tetris = Input(shape=(2, 4, 4), name="in_tetris")  # current_tetris_4x4, saved_tetris_4x4
+in_gaps = Input(shape=(1, 10), name="in_gaps")  # top_line_gaps, technically a one dimensional input
 in_one_dim = Input(shape=(16, ), name="in_one_dim")  # other one dimensional inputs
 
 # model layers
-layer_boards = Dense(1000)(in_boards)
+layer_boards = Dense(100)(in_boards)
 layer_tetris = Dense(100)(in_tetris)
 layer_gaps = Dense(10)(in_gaps)
 
 # reshape and dense to match shapes
-layer_boards = Reshape(target_shape=(1000, 240))(layer_boards)
-layer_tetris = Reshape(target_shape=(40, 40))(layer_tetris)
-layer_gaps = Reshape(target_shape=(10, 10))(layer_gaps)
+layer_boards = Reshape(target_shape=(100, 40))(layer_boards)
+layer_tetris = Reshape(target_shape=(40, 20))(layer_tetris)
+layer_gaps = Reshape(target_shape=(10, 1))(layer_gaps)
 
 layer_boards = Dense(100)(layer_boards)
 layer_tetris = Dense(100)(layer_tetris)
@@ -117,18 +122,13 @@ layer_multi_dim = concatenate([layer_boards, layer_tetris, layer_gaps], axis=1)
 layer_multi_dim = Dense(1000)(layer_multi_dim)
 
 # handle single dim layer
-layer_one_dim = Dense(500)(in_one_dim)
-
-print(layer_one_dim)
-print(layer_multi_dim)
+layer_one_dim = Dense(100)(in_one_dim)
 
 # reshape and combine the multi_dim and the single dim layers
 layer_multi_dim = Flatten()(layer_multi_dim)
-print(layer_multi_dim)
-
 layer_main = concatenate([layer_one_dim, layer_multi_dim])
-layer_main = Dense(2000)(layer_main)
-layer_main = Dense(2000)(layer_main)
+layer_main = Dense(1000)(layer_main)
+layer_main = Dense(1000)(layer_main)
 
 # outputs - corresponding to actions in the action space
 output = Dense(len(action_space), name="output", activation="relu")(layer_main)
@@ -153,11 +153,10 @@ epoch_total = 1000
 mutate_threshold = 0.5  # 0 = all model moves, 1 = all random moves
 draw_board = False  # display training actions on screen
 
-# training starts
-reward_list, inputs_list, action_taken_list = [], [], []
+# training start
+reward_list, inputs_list, action_taken_list, game_tick_index_list, lines_cleared_list = [], [], [], [], []
 epoch = 0
 time_start = time.time()
-game_tick_index_list, lines_cleared_list = [], []
 continue_train = True
 if draw_board:
     drawBoard = DrawBoard()
@@ -200,14 +199,20 @@ while continue_train:
             row[np.argmax(action_taken_list[index])] = 1
 
         # fit the model
-        model.fit(prepare_model_inputs(game_state), action_taken_list_abs, sample_weight=np.array(reward_list), verbose=1)
+        model.fit(prepare_model_inputs(inputs_list),
+                  action_taken_list_abs,
+                  sample_weight=np.array(reward_list),
+                  verbose=1)
 
         # reset the training data lists
         reward_list, inputs_list, action_taken_list = [], [], []
 
         # log progress
         epoch += 1
-        print("Epoch: {}, GameTicks: {}, LinesCleared: {}, EstTimeRemaining(s): {}".format(epoch, game.game_tick_index, game.lines_cleared, (((time.time() - time_start) / (1 - ((epoch_total - epoch) / epoch_total)))) - (time.time() - time_start)))
+        print("Epoch: {}, GameTicks: {}, LinesCleared: {}, EstTimeRemaining(s): {}".format(epoch,
+                                                                                           game.game_tick_index,
+                                                                                           game.lines_cleared,
+                                                                                           (((time.time() - time_start) / (1 - ((epoch_total - epoch) / epoch_total)))) - (time.time() - time_start)))
         game_tick_index_list.append(game.game_tick_index)
         lines_cleared_list.append(game.lines_cleared)
 
@@ -220,8 +225,10 @@ while continue_train:
 
 print("-- Training Finished -- ")
 
-print("Average Ticks Survived: {} Average Lines Cleared: {}".format(np.average(game_tick_index_list), np.average(lines_cleared_list)))
-print("Max Ticks Survived: {} Max Lines Cleared: {}".format(np.max(game_tick_index_list), np.max(lines_cleared_list)))
+print("Average Ticks Survived: {} Average Lines Cleared: {}".format(np.average(game_tick_index_list),
+                                                                    np.average(lines_cleared_list)))
+print("Max Ticks Survived: {} Max Lines Cleared: {}".format(np.max(game_tick_index_list),
+                                                            np.max(lines_cleared_list)))
 
 # save model and training data
 files_list = os.listdir("tetris_dqn_training/tetris_dqn_models")
